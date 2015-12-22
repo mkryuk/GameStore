f0a74415dfb4282b6751d40f9f2f81aa05596740
mkryuk <mkryuk@gmail.com>
2015-12-22 21:03:24 +0200

    Initial

---
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        private IGameRepository repository;
+        public GameController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ActionResult List()
+        {
+            return View(repository.Games);
+        }
+    }
+}
