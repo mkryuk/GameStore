9abf562a8850dc72faa5dc202f28c298a792270d
mkryuk <mkryuk@gmail.com>
2015-12-23 10:53:43 +0200

    - Mocked database + EF Code First repository

---
+++ GameStore.Domain/Concrete/EFGameRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Concrete
+{
+    public class EFGameRepository : IGameRepository
+    {
+        EFDbContext context = new EFDbContext();
+        public IEnumerable<Game> Games
+        {
+            get
+            { return context.Games; }
+        }
+    }
+}
--- GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GameStore.Domain.Abstract;
+using GameStore.Domain.Concrete;
 using GameStore.Domain.Entities;
 using Moq;
 using Ninject;
@@ -31,15 +32,8 @@ namespace GameStore.WebUI.Infrastructure
         }
 
         private void AddBindings()
-        {
-            var mock = new Mock<IGameRepository>();
-            mock.Setup(m => m.Games).Returns(new List<Game>
-            {
-                new Game {Name = "SimCity", Price = 1499},
-                new Game {Name = "TITANFALL", Price = 2299},
-                new Game {Name = "Battlefield 4", Price = 899.4M}
-            });
-            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+        {            
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
         }
     }
 }
